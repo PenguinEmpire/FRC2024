2511ba67f488a5dc147512cbe6f2b602940a811d
nisunku <madhavi253@gmail.com>
2024-01-22 19:49:52 -0800

    framework

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -8,24 +8,32 @@ import com.revrobotics.SparkPIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class WristSubsystem { 
+public class ShooterSubsystem { 
     private final CANSparkMax armMotor;
     private final CANSparkMax intakeMotor; 
     private final CANSparkMax shooterMotor;
 
     private final SparkPIDController armPID;
     private final SparkPIDController intakePID;
+    private final SparkPIDController shooterSpeedPID;
 
     private final RelativeEncoder armEncoder;
     private final RelativeEncoder intakeEncoder;
 
-    public WristSubsystem(int armSparkID, int intakeSparkID, int shooterSparkID) {
+    public ShooterSubsystem(int armSparkID, int intakeSparkID, int shooterSparkID) {
         armMotor = new CANSparkMax(armSparkID, CANSparkMax.MotorType.kBrushless);
         intakeMotor = new CANSparkMax(intakeSparkID, CANSparkMax.MotorType.kBrushless);
         shooterMotor = new CANSparkMax(shooterSparkID, CANSparkMax.MotorType.kBrushless);
 
-        
-        
+        armPID = armMotor.getPIDController();
+        intakePID = intakeMotor.getPIDController();
+        shooterSpeedPID = shooterMotor.getPIDController();
+
+        armEncoder = armMotor.getEncoder();
+        intakeEncoder = intakeMotor.getEncoder();
     }
+
+
+    
 
 }
