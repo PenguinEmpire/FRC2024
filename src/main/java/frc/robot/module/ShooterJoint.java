ba927b2a59882ab581c8f36ba9c919e70909b8b4
RehanNazeem <rehannazeem8@gmail.com>
2024-01-29 18:33:53 -0800

    ShooterJoint initial push

--- src/main/java/frc/robot/module/ShooterJoint.java
+++ src/main/java/frc/robot/module/ShooterJoint.java
@@ -1,5 +1,21 @@
 package frc.robot.module;
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ShooterJoint {
+    private CANSparkMax armMotor;
+
+    public ShooterJoint() {
+        // need to change ID
+        this.armMotor = new CANSparkMax( 0, CANSparkMax.MotorType.kBrushless);
+        SmartDashboard.putNumber("Arm-A Speed", 0.6);
+    }
+
+    public void moveA(boolean toggle, boolean reverse) {
+        double speed = SmartDashboard.getNumber("Arm-A Speed", 0);
+      
+        armMotor.set(speed);
     
+        
+    }
 }
