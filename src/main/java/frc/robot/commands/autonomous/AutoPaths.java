ee22d4ab63b44c1a16adbff06f5984b3dbb691ea
nisunku <madhavi253@gmail.com>
2024-02-28 09:58:29 -0800

    a

--- src/main/java/frc/robot/commands/autonomous/AutoPaths.java
+++ src/main/java/frc/robot/commands/autonomous/AutoPaths.java
@@ -1,6 +1,9 @@
 package frc.robot.commands.autonomous;
 
+import java.nio.file.Path;
+
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.pathplanner.lib.path.PathPlannerPath;
 
 import edu.wpi.first.wpilibj2.command.Command;
@@ -18,15 +21,15 @@ public class AutoPaths {
                 new PositionCommand(shooterSubsystem, intakeSubsystem, PositionCommand.Position.OUT_OF_AUTO_POSITION),
                 autoMotions.shootingClosestAutoMotion(),
                 new ParallelCommandGroup(
-                        AutoBuilder.followPath(PathPlannerPath.fromPathFile("speakerToLeft")),
+                        AutoBuilder.followPath(PathPlannerPath.fromPathFile("BspeakerToLeft")),
                         autoMotions.intakeAutoMotion()),
                 autoMotions.shootingMiddleAutoMotion(),
                 new ParallelCommandGroup(
-                        AutoBuilder.followPath(PathPlannerPath.fromPathFile("leftToMiddle")),
+                        AutoBuilder.followPath(PathPlannerPath.fromPathFile("BleftToMiddle")),
                         autoMotions.intakeAutoMotion()),
                 autoMotions.shootingMiddleAutoMotion(),
                 new ParallelCommandGroup(
-                        AutoBuilder.followPath(PathPlannerPath.fromPathFile("middleToRight")),
+                        AutoBuilder.followPath(PathPlannerPath.fromPathFile("BmiddleToRight")),
                         autoMotions.intakeAutoMotion()),
                 autoMotions.shootingMiddleAutoMotion()
 
@@ -37,31 +40,35 @@ public class AutoPaths {
             AutoMotions autoMotions) {
         return new SequentialCommandGroup(
                 new PositionCommand(shooterSubsystem, intakeSubsystem, PositionCommand.Position.OUT_OF_AUTO_POSITION),
-                AutoBuilder.followPath(PathPlannerPath.fromPathFile("wallToSpeaker")),
+                AutoBuilder.followPath(PathPlannerPath.fromPathFile("BwallToSpeaker")),
                 autoMotions.shootingClosestAutoMotion(),
                 new ParallelCommandGroup(
                         autoMotions.intakeAutoMotion(),
-                        AutoBuilder.followPath(PathPlannerPath.fromPathFile("rightSpeakertoRight"))),
+                        AutoBuilder.followPath(PathPlannerPath.fromPathFile("BrightSpeakertoRight"))),
                 autoMotions.shootingMiddleAutoMotion(),
                 new ParallelCommandGroup(
-                    AutoBuilder.followPath(PathPlannerPath.fromPathFile("rightToBackRight")),
+                    AutoBuilder.followPath(PathPlannerPath.fromPathFile("BrightToBackRight")),
                     new SequentialCommandGroup(
                         new WaitCommand (0.5),
                         autoMotions.intakeAutoMotion()
                     )   
                 ),
-                AutoBuilder.followPath(PathPlannerPath.fromPathFile("backRightToShoot")),
+                AutoBuilder.followPath(PathPlannerPath.fromPathFile("BbackRightToShoot")),
                 autoMotions.shootingFarAutoMotion(),
                 new ParallelCommandGroup(
-                    AutoBuilder.followPath(PathPlannerPath.fromPathFile("shootToBackSecond")),
+                    AutoBuilder.followPath(PathPlannerPath.fromPathFile("BshootToBackSecond")),
                     new SequentialCommandGroup(
                         new WaitCommand (1.5),
                         autoMotions.intakeAutoMotion()
                     )
                 )
-
-
-
         );
     }
+
+    public static Command redCenterFourPiece (IntakeSubsystem intakeSubsystem, ShooterSubsystem shooterSubsystem,
+            AutoMotions autoMotions) {
+                return new SequentialCommandGroup (
+                    new PositionCommand(shooterSubsystem, intakeSubsystem, PositionCommand.Position.OUT_OF_AUTO_POSITION)
+                );
+            }
 }
