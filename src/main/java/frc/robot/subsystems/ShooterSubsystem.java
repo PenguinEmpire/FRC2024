21e6235f9aa4eabde34f820cd52c10ad39629053
Aidan <30511407+aidanwd@users.noreply.github.com>
2024-03-04 18:58:29 -0800

    some pid tuning on intake

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -36,7 +36,7 @@ public class ShooterSubsystem extends SubsystemBase {
 
     public ShooterSubsystem(int feederID, int shooterID, ControlInput controlInput, VisionSubsystem vs) {
         arm = new Joint("shooterArm", 11, 0.7, 0, 0, 0, 0, -0.30, 0.30, true, null, 0, false);
-        shooter = new Joint("shooterEnt", 20, 0.95, 0.0001, 0, 0, 0.001, -0.27, 0.27, false, null, 0, false);
+        shooter = new Joint("shooterEnt", 20, 0.95, 0.01, 0.2, 0, 0, -0.27, 0.27, false, null, 0, false);
 
         feederMotor = new CANSparkMax(feederID, CANSparkMax.MotorType.kBrushless);
         shooterMotor = new CANSparkMax(shooterID, CANSparkMax.MotorType.kBrushless);
