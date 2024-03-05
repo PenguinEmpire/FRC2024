4c2ebd327a314dfc386387b7729601eacbdb5464
nisunku <madhavi253@gmail.com>
2024-03-05 09:35:23 -0800

    climbing basics

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -3,6 +3,8 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimberSubsystem extends SubsystemBase {
@@ -13,6 +15,13 @@ public class ClimberSubsystem extends SubsystemBase {
 
     public ClimberSubsystem (int sparkID) {
         climberMotor = new CANSparkMax(sparkID, MotorType.kBrushed);
+    }
+
+    public Command runClimberMotor() {
+        return Commands.runEnd(
+            () -> climberMotor.set(0.5),
+            () -> climberMotor.set(0)
+        );
     }
 
 }
